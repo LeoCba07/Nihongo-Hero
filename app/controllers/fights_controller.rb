c1fd83c61b0f8e735c49f072ebbbdfdd7e8b9056
Haley Banfield <HaleyKBanfield@gmail.com>
2025-11-26 14:26:01 +0900

    Added logic for a create method in the Fights controller

--- app/controllers/fights_controller.rb
+++ app/controllers/fights_controller.rb
@@ -4,7 +4,17 @@ class FightsController < ApplicationController
   end
 
   def create
-    @fight = Fight.create(user: current_user, status: 'active')
+    enemy = Enemy.all.sample
+
+    # HALEY: I've whipped this bad create method up to get to the fight screen so I can work on it.
+    @fight = Fight.create(
+      status: 'active',
+      user: current_user,
+      enemy: enemy,
+      player_hitpoints: current_user.hitpoints,
+      enemy_hitpoints: enemy.hitpoints,
+      story_level_id: 1
+    )
 
     if @fight.save
       redirect_to fight_path(@fight)
