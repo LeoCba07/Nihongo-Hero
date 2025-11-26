ece97767276275e3d2b1bca41edb49fac61f2bbf
Haley Banfield <HaleyKBanfield@gmail.com>
2025-11-26 11:49:56 +0900

    Added the basic routing and rerouting from the login page to the map to the fight.

--- app/controllers/fights_controller.rb
+++ app/controllers/fights_controller.rb
@@ -1,2 +1,21 @@
 class FightsController < ApplicationController
+  def show
+    @fight = Fight.find(params[:id])
+  end
+
+  def create
+    @fight = Fight.create(user: current_user, status: 'active')
+
+    if @fight.save
+      redirect_to fight_path(@fight)
+    else
+    flash[:alert] = @fight.errors.full_messages.join(", ")
+    redirect_to map_path
+    end
+  end
+
+  private
+
+  def fight_params
+  end
 end
