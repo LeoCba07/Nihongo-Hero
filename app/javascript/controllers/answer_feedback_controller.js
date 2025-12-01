1c9ded91dc5e4e327921e7a2f9d5489a19748a55
Leandro Trabucco <leandrotrabucco@hotmail.com>
2025-12-01 17:26:51 +0900

    obtained a functional health bar animation decrease but damages are hardcoded

--- app/javascript/controllers/answer_feedback_controller.js
+++ app/javascript/controllers/answer_feedback_controller.js
@@ -6,10 +6,14 @@ export default class extends Controller {
   static targets = ["answer", "player", "enemy", "enemyHealthbar", "enemyHealthText", "playerHealthbar", "playerHealthText"]
   // Creates getter for @question.correct_index, @damage_dealt & @damage_received
   static values = {
-    correctIndex: Number,
-    damageDealt: Number,
-    damageReceived: Number
-  }
+  correctIndex: Number,
+  damageDealt: Number,
+  damageReceived: Number,
+  playerHp: Number,
+  playerMaxHp: Number,
+  enemyHp: Number,
+  enemyMaxHp: Number
+}
 
   feedback(event) {
     // Get the selected answer with index dataset on each button
@@ -22,6 +26,7 @@ export default class extends Controller {
     // Because if I do the timeout and then try to send the event closest form it can't find it anymore
     const form = event.currentTarget.closest('form');
 
+    // Testing logs do not delete pls
     // console.log("Button clicked!")
     // console.log("Correct index is:", this.correctIndexValue)
     // console.log("Is the answer correct?:", isCorrect);
@@ -29,6 +34,10 @@ export default class extends Controller {
     // console.log("Damage received:", this.damageReceivedValue);
     // console.log("Player target:", this.playerTarget)
     // console.log("Enemy target:", this.enemyTarget)
+    console.log("Player HP:", this.playerHpValue);
+    console.log("Enemy HP:", this.enemyHpValue);
+    console.log("Player Max HP:", this.playerMaxHpValue);
+    console.log("Enemy Max HP:", this.enemyMaxHpValue);
 
     event.preventDefault();
 
@@ -41,10 +50,24 @@ export default class extends Controller {
       // If selected answer is correct -> highlight green
       event.currentTarget.classList.add("correct", "selected");
 
+      // Create a div for dmg, add css, add dmg dealt text and append to enemy sprite
       const damageEl = document.createElement("div");
       damageEl.classList.add("damage-number");
       damageEl.textContent = `-${this.damageDealtValue}`;
       this.enemyTarget.appendChild(damageEl);
+
+      // Calculate new HP
+      const newEnemyHp = this.enemyHpValue - this.damageDealtValue
+
+      // Calculate percentage
+      const newPercent = (newEnemyHp / this.enemyMaxHpValue) * 100
+
+      // Update the healthbar width
+      this.enemyHealthbarTarget.style.width = `${newPercent}%`
+
+      // Update the text
+      this.enemyHealthTextTarget.textContent = `${newEnemyHp} HP`
+
 
     } else {
       // Find correct button if the answer was wrong
@@ -59,10 +82,16 @@ export default class extends Controller {
       // Additionally correct answer -> highlight green
       event.currentTarget.classList.add("incorrect", "selected");
       correctButton.classList.add("correct");
+
+      // Same block as up before
+      const newPlayerHp = this.playerHpValue - this.damageReceivedValue
+      const newPercent = (newPlayerHp / this.playerMaxHpValue) * 100
+      this.playerHealthbarTarget.style.width = `${newPercent}%`
+      this.playerHealthTextTarget.textContent = `${newPlayerHp} HP`
     }
 
     // Set delay to be longer if answer was incorrect
-    const delay = isCorrect ? 1700 : 3300
+    const delay = isCorrect ? 1700 : 3200
 
     // Wait and then submit the form reference
     setTimeout(() => {
